f527964dd154486103deea5519a2e0d61138d8e4
Wojciech Repinski <wrepinski@gmail.com>
2019-01-10 20:19:09 +0100

    Update otrs_install.sh

--- otrs_install.sh
+++ otrs_install.sh
@@ -4,10 +4,11 @@ mkdir -p /opt/otrs
 
 curl -o /opt/otrs-$OTRS_VERSION.zip http://ftp.otrs.org/pub/otrs/otrs-$OTRS_VERSION.zip
 
+cd /opt
+
 unzip otrs-$OTRS_VERSION.zip
 
-rm /otrs-$OTRS_VERSION.zip 
+rm otrs-$OTRS_VERSION.zip 
 
-mv /otrs-$OTRS_VERSION /opt/
 
 
