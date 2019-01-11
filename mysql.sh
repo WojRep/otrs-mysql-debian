d3021e4f5aaa2c67adbebc9010e6ee0069cc6a9f
Wojciech Repinski <wrepinski@gmail.com>
2019-01-11 23:32:12 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -5,23 +5,17 @@
 if [ "$OTRS_INSTALL" == "yes"  ]; then
 	rm -rf /var/lib/mysql
 	mkdir -p /var/lib/mysql
-
-#	mysql_install_db
 	mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
-#
 	touch /var/lib/mysql/firsttime
-#	/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=root &
-/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=mysql &
-
-#mysql_upgrade
+	/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=mysql &
 
 	while :
 	 do
 	  if [ -e "/var/run/mysqld/mysqld.pid" ]; then
 		echo $DB_ROOT_PASSWORD
-		mysqladmin -u root password '$DB_ROOT_PASSWORD'
-		#mysqladmin -u root -h localhost password '$DB_ROOT_PASSWORD'
-		#echo "UPDATE mysql.user SET plugin = '' WHERE user = 'root';" | mysql -uroot -hlocalhost -p$DB_ROOT_PASSWORD
+		echo „UPDATE mysql.user SET plugin = '';FLUSH PRIVILEGES;”| mysql
+		echo „GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY ‚$DB_ROOT_PASSWORD’;GRANT ALL PRIVILEGES ON *.* TO 'root’@‚localhost’ IDENTIFIED BY ‚$DB_ROOT_PASSWORD’;FLUSH PRIVILEGES;”| mysql
+
    	 if [ $? = 0 ]; then
   	      break
  	   fi
@@ -29,9 +23,5 @@ if [ "$OTRS_INSTALL" == "yes"  ]; then
 	  fi
 	  sleep 2
 	done
-	
-	#service mariadb stop
-
-
 fi
 
