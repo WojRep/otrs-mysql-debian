c45f0efe02a4bbf4107c1ff3430d8f1839916d65
Wojciech Repinski <wrepinski@gmail.com>
2019-01-11 09:19:56 +0100

    1

--- mysql.sh
+++ mysql.sh
@@ -3,14 +3,31 @@
 
 
 if [ "$OTRS_INSTALL" == "yes"  ]; then
-rm -rf /var/lib/mysql
-mkdir -p /var/lib/mysql
-touch /var/lib/mysql/firsttime
-mysql_install_db
+	rm -rf /var/lib/mysql
+	mkdir -p /var/lib/mysql
+	touch /var/lib/mysql/firsttime
+	mysql_install_db
 
-mysqladmin -u root password '$DB_ROOT_PASSWORD'
-mysqladmin -u root -h localhost password '$DB_ROOT_PASSWORD'
 
-echo "UPDATE mysql.user SET plugin = '' WHERE user = 'root';" | mysql -uroot -hlocalhost -p$DB_ROOT_PASSWORD
+	/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=root &
+
+
+	while :
+	 do
+	  if [ -e "/var/run/mysqld/mysqld.pid" ]; then
+		mysqladmin -u root password '$DB_ROOT_PASSWORD'
+		mysqladmin -u root -h localhost password '$DB_ROOT_PASSWORD'
+		echo "UPDATE mysql.user SET plugin = '' WHERE user = 'root';" | mysql -uroot -hlocalhost -p$DB_ROOT_PASSWORD
+   	 if [ $? = 0 ]; then
+  	      break
+ 	   fi
+ 	   echo "Change password failed. retry"
+	  fi
+	  sleep 2
+	done
+	
+	service mariadb stop
+
+
 fi
 
