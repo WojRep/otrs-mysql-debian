dffe41d9afffe9078b171ec6a6552f7863ca7163
Wojciech Repinski <wrepinski@gmail.com>
2019-01-11 23:52:33 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
-
+echo "#################################\n"
+echo "##                    \n"
+echo "##  ".$DB_ROOT_PASSWORD.”   \n”
+echo "##                    \n"
+echo "#################################\n"
 
 if [ "$OTRS_INSTALL" == "yes"  ]; then
 	rm -rf /var/lib/mysql
@@ -12,9 +16,8 @@ if [ "$OTRS_INSTALL" == "yes"  ]; then
 	while :
 	 do
 	  if [ -e "/var/run/mysqld/mysqld.pid" ]; then
-		echo $DB_ROOT_PASSWORD
-		echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';GRANT ALL PRIVILEGES ON *.* TO 'root’@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';FLUSH PRIVILEGES;"| mysql
-		echo "UPDATE mysql.user SET plugin = '';FLUSH PRIVILEGES;"| mysql
+		echo "USE mysql;UPDATE mysql.user SET plugin = 'mysql_native_password';FLUSH PRIVILEGES;"| mysql		echo "USE mysql;GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';FLUSH PRIVILEGES;"| mysql
+		
    	 if [ $? = 0 ]; then
   	      break
  	   fi
