292d3ac6b89f7741fbe9395e7419ba65c5ab2204
Wojciech Repinski <wrepinski@gmail.com>
2019-01-12 11:03:00 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -5,17 +5,22 @@ echo "##                    "
 echo "##  ".$DB_ROOT_PASSWORD
 echo "##                    "
 echo "#################################"
-
+echo "Start mysql.sh" > /tmp/mysql.log
 if [ "$OTRS_INSTALL" == "yes"  ]; then
+	echo "$OTRS_INSTALL" >> /tmp/mysql.log
 	rm -rf /var/lib/mysql
 	mkdir -p /var/lib/mysql
+	echo "Initializing MySQL" >> /tmp/mysql.log
 	mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
 	touch /var/lib/mysql/firsttime
+	echo "Starting MySQLd" >> /tmp/mysql.log
 	/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=mysql &
 
 	while :
 	 do
 	  if [ -e "/var/run/mysqld/mysqld.pid" ]; then
+		echo "/var/run/mysqld/mysqld.pid" >> /tmp/mysql.log
+		echo "Start update privileges and set root password" >> /tmp/mysql.log
 		echo "USE mysql;GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';"| mysql
 		echo "USE mysql;UPDATE mysql.user SET plugin = 'mysql_native_password'"| mysql
 		echo "UPDATE mysql.user SET authentication_string = PASSWORD('$DB_ROOT_PASSWORD') WHERE User = 'root';;FLUSH PRIVILEGES;"| mysql
@@ -24,8 +29,11 @@ if [ "$OTRS_INSTALL" == "yes"  ]; then
   	      break
  	   fi
  	   echo "Change password failed. retry"
+	   echo "Change password failed. retry" >> /tmp/mysql.log
 	  fi
 	  sleep 2
+	  echo "sleep 2" >> /tmp/mysql.log
 	done
 fi
 
+echo "End mysql.sh" >> /tmp/mysql.log
