b0d225cef55b0e2b69667a59e0ffe24584eae3bd
Wojciech Repinski <wrepinski@gmail.com>
2019-01-12 10:41:23 +0100

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -16,11 +16,10 @@ if [ "$OTRS_INSTALL" == "yes"  ]; then
 	while :
 	 do
 	  if [ -e "/var/run/mysqld/mysqld.pid" ]; then
-		echo "plugins\n"
-		echo "USE mysql;UPDATE mysql.user SET plugin = 'mysql_native_password';FLUSH PRIVILEGES;"| mysql
-		echo "Grant\n"
 		echo "USE mysql;GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';"| mysql
-		echo "USE mysql;GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';FLUSH PRIVILEGES;"| mysql
+		echo "USE mysql;UPDATE mysql.user SET plugin = 'mysql_native_password'"| mysql
+		echo "UPDATE mysql.user SET authentication_string = PASSWORD('$DB_ROOT_PASSWORD') WHERE User = 'root';;FLUSH PRIVILEGES;"| mysql
+		#echo "USE mysql;GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';FLUSH PRIVILEGES;"| mysql
    	 if [ $? = 0 ]; then
   	      break
  	   fi
